20ca22cc602a1610c9c42081ca5c9d754d3bd475
beatriz_g <beatrizgue20@gmail.com>
2025-01-27 13:27:02 -0800

    Added button to remove picture

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ var cat = document.querySelector(".cat");
 
 // Adding event listener and event handler for button to listen for a click event
 button.addEventListener("click", function() {
-    // Adding the show class
-    cat.classList.add("show");
-});
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+    }
+}); 
